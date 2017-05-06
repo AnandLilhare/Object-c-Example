44e9a4689a206fc559b946949cae8dac7cdc6106
Anand Lilhare <anandlilhare69@gmail.com>
2017-05-06 15:47:39 +0530

    Nsstring example

--- NSStringDemo/NSStringDemo/main.m
+++ NSStringDemo/NSStringDemo/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *str1 = @"Hello";
+        NSString *str2 = @"Anand";
+        NSString *str3;
+        int  len ;
+        
+        /* uppercase string */
+        str3 = [str2 uppercaseString];
+        NSLog(@"Uppercase String :  %@\n", str3 );
+       
+        /* concatenates str1 and str2 */
+        str3 = [str1 stringByAppendingFormat:@" World"];
+        NSLog(@"Concatenated string:   %@\n", [str3 uppercaseString] );
+        
+        /* total length of str3 after concatenation */
+        len = [str3 length];
+        NSLog(@"Length of Str3 :  %d\n", len );
+        
+        /* InitWithFormat */
+        str3 = [[NSString alloc] initWithFormat:@"%@ %@",str1,str2];
+        NSLog(@"Using initWithFormat:   %@\n", str3 );
+        
     }
     return 0;
 }
